fe459f40d7915f0780ce85a30b22e5d1dc4c5050
Ilodi2k <ilodi@DESKTOP-5Q6LLNK.localdomain>
2022-09-22 15:25:02 +0200

    test

--- src/App.js
+++ src/App.js
@@ -8,16 +8,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Das ist meine PWA-Test App
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input>
+        </input>
+        <br></br>
+        <select>
+          <option>Test1</option>
+          <option>Test2</option>
+          <option>Test3</option>
+        </select>
       </header>
     </div>
   );
