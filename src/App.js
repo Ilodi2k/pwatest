9785ffe9c5144376ccd68970427aaae4ac31824d
Ilodi2k <ilodi@DESKTOP-5Q6LLNK.localdomain>
2022-10-12 15:03:55 +0200

    ey

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  const [textInput, setTextInput] = useState()
+
+  const [todos, setTodos] = useState([""]);
+
+  const handleSubmitTodo = (textInput, todos) => {
+    if (todos === [""]) {
+      setTodos(textInput)
+    }
+
+    if (textInput =! "") {
+      const updatedTodos = [...todos, textInput] 
+      setTodos(updatedTodos)
+    }
+  }
+
+  const handleTextInput = (e) => {
+    setTextInput(e.value)
+  }
+
+  const handleDeleteTodo = () => {
+    console.log("delete")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Das ist meine PWA-Test App
-        </p>
-        <input>
-        </input>
-        <br></br>
-        <select>
-          <option>Test1</option>
-          <option>Test2</option>
-          <option>Test3</option>
-        </select>
-      </header>
+      <div style={{display: "flex", flexDirection:"column", alignItems:"center"}}>
+        {todos.map((todo) => (
+          <div style={{display: "flex", gap:"20px"}}>
+            <div>{todo}</div>
+            <button onClick={handleDeleteTodo}>❌</button>
+          </div> 
+        ))}
+      </div>
+      <br></br>
+      <div style={{display: "flex", justifyContent:"center"}}>
+        <div >
+        <input onChange={(e) => handleTextInput}></input>
+        </div>
+        <div>
+          <button onClick={handleSubmitTodo}>➕</button>
+        </div>
+      </div>
     </div>
   );
 }
